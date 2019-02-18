34dae6c98b4d827387ccb1371a7b29937cea4561
kjain <Kapil.Jain@HMCTS.NET>
2019-02-18 12:52:59 +0000

    sslfix

--- src/gatling/simulations/uk/gov/hmcts/jui/sscs/scenario/JUILogin.scala
+++ src/gatling/simulations/uk/gov/hmcts/jui/sscs/scenario/JUILogin.scala
@@ -14,37 +14,34 @@ object JUILogin {
   val MaxThinkTime = Environment.maxThinkTime
   val JUIBaseUrl = scala.util.Properties.envOrElse("URL_TO_TEST", Environment.URL_TO_TEST).toLowerCase()
 
-  val submitLogin = exec(http("TC02_JUI_SubmitLogin")
-    .post(IdamJUIURL + "/login?response_type=code&client_id=juiwebapp&redirect_uri=${redirect_uri}").disableFollowRedirect
-    //.post(IdamJUIURL + "/login?response_type=code&client_id=juiwebapp&redirect_uri="+JUIBaseUrl+"/oauth2/callback")
+  val submitLogin = 	exec(http("TC02_JUI_SubmitLogin")
+    .post(IdamJUIURL + "/login?response_type=code&client_id=juiwebapp&redirect_uri=" + JUIBaseUrl + "/oauth2/callback").disableFollowRedirect
     .formParam("username", "${SSCSUserName}")
     .formParam("password", "${SSCSUsrPwd}")
-    //.formParam("continue", "${continue}")
     .formParam("continue", JUIBaseUrl + "/oauth2/callback")
     .formParam("upliftToken", "")
-    .formParam("response_type", "${response_type}")
+    .formParam("response_type", "code")
     .formParam("_csrf", "${csrftoken}")
-    //.formParam("redirect_uri", "${redirect_uri}")
-    .formParam("redirect_uri", JUIBaseUrl +"/oauth2/callback")
+    .formParam("redirect_uri", JUIBaseUrl + "/oauth2/callback")
     .formParam("client_id", "juiwebapp")
     .formParam("scope", "")
     .formParam("state", "")
     .check(headerRegex("Location", "(?<=code=)(.*)").saveAs("authCode"))
     .check(status.in(200,302)))
+    .pause(MinThinkTime, MaxThinkTime)
 
-    .pause(4)
     .exec((http("TC02_JUI_SubmitLogin_authCode")
-      .get("/oauth2/callback?code=${authCode}"))
+      .get("/oauth2/callback?code=${authCode}")
       .check(regex("""href="/case/SSCS/Benefit/(.*?)/casefile""").findAll.saveAs("P_cases"))
-      .check(regex("""href="/case/SSCS/Benefit/(.*?)/casefile""").count.gte(1).saveAs("pickCaseCounts")))
-    .pause(4)
+      .check(regex("""href="/case/SSCS/Benefit/(.*?)/casefile""").count.gte(1).saveAs("pickCaseCounts"))))
+
 
     .pause(MinThinkTime, MaxThinkTime)
 
-    .exec(http("TC02_JUI_SubmitLogin_bold-a2452cb66f-v1.woff2")
+   /* .exec(http("TC02_JUI_SubmitLogin_bold-a2452cb66f-v1.woff2")
       .get("/assets/fonts/bold-a2452cb66f-v1.woff2")
       .resources(http("TC02_JUI_SubmitLogin_light-f38ad40456-v1.woff2")
-        .get("/assets/fonts/light-f38ad40456-v1.woff2")))
+        .get("/assets/fonts/light-f38ad40456-v1.woff2")))*/
 
 
     .exec(session => {
